c222e4411f408dfaf3c765895c8076196bd95b19
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-01-19 22:11:22 +0530

    routes and models added and DB connected

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('working')
-})
+const cookieParser = require('cookie-parser');
+const path = require('path')
+
+const db = require("./config/databaseConnection")
+const usersRouter = require("./routes/usersRouter")
+const ownersRouter = require("./routes/ownersRouter")
+const productsRouter = require("./routes/productsRouter")
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.use("/users", usersRouter)
+app.use("/owners", ownersRouter)
+app.use("/products", productsRouter)
 
 const PORT = 3000
 app.listen(PORT, () => {
